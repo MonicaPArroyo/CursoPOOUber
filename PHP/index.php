99c1fe9954485b1fd2b5c1b60a3f3d81dddd9815
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2022-01-23 22:27:15 -0600

    Herencia en Java y PHP

--- PHP/index.php
+++ PHP/index.php
@@ -1,7 +1,11 @@
 <?php
 
 require_once('Car.php');
+require_once('uberX.php');
+require_once('uberPool.php');
+require_once('uberBlack.php');
+require_once('uberVan.php');
 require_once('Account.php');
 
-$car = new Car("AW456", new Account("Andres Herrera", "AMS123"));
-$car->PrintDataCar();
+$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
+$uberX->printDataCar();
