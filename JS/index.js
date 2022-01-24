e4e868d0d3dcfd4230b677c3009c531e9b08cf72
Mónica P. Arroyo <monicaparroyo7@gmail.com>
2022-01-23 19:52:37 -0600

    Constructores de java y javascript

---
+++ JS/index.js
@@ -0,0 +1,3 @@
+let car = new Car("AW456", new Account("Andres Herrera", "ADH123"))
+car.passenger = 4;
+car.printDataCar();
--- Java/main.java
+++ Java/main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    
+    Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
+    car.printDataCar();
   }
 }
